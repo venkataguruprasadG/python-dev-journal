3549273950654eef8e6aeed81c0122d442d7d3da
venkataguruprasadG <venkatagp27@gmail.com>
2025-10-08 17:20:49 +0530

    while loop ex 4

---
+++ loops.py
@@ -0,0 +1,12 @@
+#Write a Python script that prints all even numbers from 2 to 20 using both a for loop and a while loop
+
+print("using for loop")
+
+for i in range(2,21,2):
+    print(i)
+
+print("using while loop")
+
+while i <= 20:
+    print(i)
+    i += 2
