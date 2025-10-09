0905205a5688dd4ae3a864368746b925f44f4dd4
venkataguruprasadG <venkatagp27@gmail.com>
2025-10-09 21:29:30 +0530

    lists exercise.

---
+++ lists.py
@@ -0,0 +1,20 @@
+"""
+Coding Exercise
+Write a script that:
+
+Creates a list with several numbers.
+
+Adds a number to the end of the list.
+
+Inserts a number at the start.
+
+Removes a specific number.
+
+Prints the final list and its length.
+"""
+
+sev_num_list = [10, 20, 30, 40, 50]
+sev_num_list.append(60) # Adds 60 to the end of the list
+sev_num_list.insert(0,5) # Inserts 5 at the start of the list
+sev_num_list.remove(30) # Removes the number 30 from the list
+print(sev_num_list) # Prints the final list
