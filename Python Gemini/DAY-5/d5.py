795147d81c478cce2d1285ff5f443520089035ca
venkataguruprasadG <venkatagp27@gmail.com>
2025-10-13 21:24:37 +0530

    dictionaries.

---
+++ Python Gemini/DAY-5/d5.py
@@ -0,0 +1,19 @@
+"""
+Your Task (Dictionaries):
+
+Create a Dictionary named user_profile with two key-value pairs:
+
+Key: "username", Value: "coder_guy"
+
+Key: "role", Value: "Engineer"
+
+Write a single line of code to change the value associated with the "role" key to "Senior Engineer".
+
+Write a single line of code to print the value of the "username" key.
+"""
+
+user_profile = {"username" : "coder_guy", "role" : "Engineer"}
+
+user_profile["role"] = "Senior Engineer"
+
+print(user_profile["username"])
