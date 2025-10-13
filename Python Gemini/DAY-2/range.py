39eaa1d40e0cb44e8caf9c14c066fbd3053df40b
venkataguruprasadG <venkatagp27@gmail.com>
2025-10-13 13:00:45 +0530

    day 2 range.

---
+++ Python Gemini/DAY-2/range.py
@@ -0,0 +1,10 @@
+"""
+Your First Task for Day 2:
+
+We're going to use the Python function range(), which generates a sequence of numbers.
+
+Write a simple for loop that prints every number from 1 up to (but not including) 6.
+"""
+
+for i in range(6):
+    print(i)
