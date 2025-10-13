d528984ea13e5b119269935bc4e5aed551280b91
venkataguruprasadG <venkatagp27@gmail.com>
2025-10-13 11:36:08 +0530

    Gemini roadmap DAy 1

---
+++ Python Gemini/day1.py
@@ -0,0 +1,13 @@
+"""
+our Task: Write the Python code to create a variable for each of the following pieces of data, naming the variable appropriately.
+
+A person's age (e.g., 25)
+
+The price of a product (e.g., 99.99)
+
+A website's title (e.g., "My Blog")
+"""
+
+age = 25
+price = 99.99
+website_title = "My Blog"
