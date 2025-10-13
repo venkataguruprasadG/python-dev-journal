8fd415712b232dc42f90eee7c968f338bf952385
venkataguruprasadG <venkatagp27@gmail.com>
2025-10-13 11:39:44 +0530

    day 1 topic 2

---
+++ Python Gemini/day1_topic2.py
@@ -0,0 +1,14 @@
+"""
+Imagine a scenario where a user buys two products.
+
+Create a new variable, quantity, and set its value to 2 (as an integer).
+
+Write a single Python expression to calculate the total_cost by multiplying the price variable you already created by the new quantity variable.
+
+Print the result of total_cost.
+"""
+
+quantity = 2
+price = 99.99 # This is the variable told to be taken from the previous code snippet
+total_cost = price * quantity
+print(total_cost)
