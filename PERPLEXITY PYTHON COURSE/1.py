716c643e29fa2d944714d6a42d2f90e67ddf7699
venkataguruprasadG <venkatagp27@gmail.com>
2025-10-16 15:45:12 +0530

    fresh starting and variables challenge completed.

--- PERPLEXITY PYTHON COURSE/1.py
+++
@@ -1,3 +0,0 @@
-fav_food = 'Dhosa'
-no = 5
-print(f"i like to eat {fav_food} {no} times a week")
---
+++ PERPLEXITY PYTHON COURSE/varialbes.py
@@ -0,0 +1,29 @@
+"""
+💪 Coding Challenge
+Task:
+
+Create variables to represent the following:
+
+Your name
+
+Your age
+
+Your height in meters
+
+Whether you are a student or not
+
+Print a short self-introduction using these variables. 
+Example: "Hi, I'm Aisha, 25 years old, 1.70m tall, and a student: True."
+
+Use the type() function to print the data type of each variable.
+"""
+
+name = "Vasudev Krishna"
+
+age = 27
+
+height = 6.2
+
+IsStudent = True
+
+print(f"Hi, I'm {name}, {age} years old, {height}m tall, and a student: {IsStudent} ")
