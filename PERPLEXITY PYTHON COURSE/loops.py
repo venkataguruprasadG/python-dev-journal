7a3ec9719d9a556d850fe5677e662597df99c1fd
venkataguruprasadG <venkatagp27@gmail.com>
2025-10-16 17:30:57 +0530

    for loops

---
+++ PERPLEXITY PYTHON COURSE/loops.py
@@ -0,0 +1,10 @@
+"""
+Coding Challenge:
+Create a list of your top 5 favorite movies or books.
+
+Use a for loop to print each item with the prefix "I like " before the item name.
+"""
+
+fav_movies = ["Magadheera","Bahubali","Kurukshetra"]
+for i in fav_movies:
+    print(f"I like {i}")
