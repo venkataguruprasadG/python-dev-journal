d7ebfe6b2f2adf853d55753185a85a3994138da2
venkataguruprasadG <venkatagp27@gmail.com>
2025-10-31 09:12:00 +0530

    entered function for main logic arithmetic execution and kept arguemnts for it.

---
+++ Console_Calculator/main_logic.py
@@ -0,0 +1,10 @@
+def main_logic(num1,num2,opr):
+    if opr == "+":
+        return num1 + num2
+    elif opr == "-":
+        return num1 - num2
+    elif opr == "*":
+        return num1 * num2
+    elif opr == "/":
+        return num1 / num2
+    
