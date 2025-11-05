b71475c1460b7a1d77eb2a52f156fb647b873807
venkataguruprasadG <venkatagp27@gmail.com>
2025-11-05 13:28:33 +0530

    updated the readme file about project 4.

--- word_guessing_game/main4.py
+++ word_guessing_game/main4.py
@@ -8,32 +8,56 @@ helping the user to either complete the word or lose the game based on their gue
 
 import random
 
-print("Welcome to the word guessing game...!")
+print("Welcome to the Word Guessing Game!")
 name = input("Enter your name: ")
-print(f"Good luck! {name}")
+print(f"Good Luck, {name}")
 
 words = [
-    "hello",
+    "guru",
     "krishna",
-    "madhusudhana",
-    "keshava",
     "narayana",
-    "janardhana",
-    "vasudeva",
+    "madhava",
+    "kamalanayana",
+    "bahubali",
+    "nataraj",
+    "kali",
 ]
 
 word = random.choice(words)
-
 guesses = 10
-
 word_length = len(word)
+dashed_str = ["_"] * word_length
+guessed_ltrs = []
 
-dashed_str = ""
-for i in range(word_length):
-    dashed_str += "_"
+print("Guess the characters of the word.")
+print(" ".join(dashed_str))
 
-dashed_str = list(dashed_str)
+while guesses > 0:
+    user_guess = input("Enter a character: ")
 
-print("Guess the characters...")
-print(dashed_str)
-# while guesses > 0:
+    if len(user_guess) != 1 or not user_guess.isalpha():
+        print("Please enter only one valid character.")
+        continue
+
+    if user_guess in guessed_ltrs:
+        print("you already guessed this letter. Try another one.")
+        continue
+
+    guessed_ltrs.append(user_guess)
+
+    if user_guess in word:
+        print("✅ Correct guess")
+        for i in range(word_length):
+            if word[i] == user_guess:
+                dashed_str[i] = user_guess
+    else:
+        guesses -= 1
+        print(f"❌ Wrong guess! You have {guesses} guesses left.")
+
+    print(" ".join(dashed_str))
+
+    if "_" not in dashed_str:
+        print(f"Congratulations {name}! You have guessed the word {word} correctly.")
+        break
+else:
+    print(f"\n😢 Sorry {name}, you ran out of guesses. The word was '{word}'.")
